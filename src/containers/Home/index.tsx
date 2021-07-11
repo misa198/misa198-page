bb536bb3a150c2d2951956f27afc579c87a26970
Thanh Vu <csnsjh@gmail.com>
2021-07-12 05:39:18 +0700

    home slice

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,8 +1,16 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 import About from "src/containers/Home/components/About";
+import { fetchPinnedRepositories } from "src/store/thunks/home.thunk";
 
 const Home: FC = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchPinnedRepositories());
+  }, [dispatch]);
+
   return (
     <>
       <About />
--- src/store/thunks/home.thunk.ts
+++ src/store/thunks/home.thunk.ts
@@ -3,10 +3,10 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { getPinnedRepositories } from "src/services/github.service";
 import { PinnedRepository } from "src/types/PinnedRepository";
 
-export const fetchPinnedRepositories = createAsyncThunk<
-  PinnedRepository[],
-  string
->("home/fetchPinnedRepositories", async () => {
-  const res = await getPinnedRepositories();
-  return res;
-});
+export const fetchPinnedRepositories = createAsyncThunk<PinnedRepository[]>(
+  "home/fetchPinnedRepositories",
+  async () => {
+    const res = await getPinnedRepositories();
+    return res;
+  }
+);
