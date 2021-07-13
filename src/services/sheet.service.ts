a8e70879553b801d766df538095e213e9804b8e4
Thanh Vu <csnsjh@gmail.com>
2021-07-13 21:32:36 +0700

    add time to contact

--- src/services/sheet.service.ts
+++ src/services/sheet.service.ts
@@ -5,7 +5,10 @@ import { Contact } from "src/types/Contact";
 export async function postContact(value: Contact): Promise<AxiosResponse> {
   const res = await axios.post(
     "https://sheet.best/api/sheets/fca2036a-e839-4dd2-ad56-ad2d3c2c9976",
-    value
+    {
+      ...value,
+      time: new Date(),
+    }
   );
   return res;
 }
