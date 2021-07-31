fc4668e483cbcc73b8bb367ba3d14160d20ace4a
Thanh Vu <csnsjh@gmail.com>
2021-07-31 17:53:51 +0700

    reload when location has been changed

--- components/Pages/Blog/Comments/index.tsx
+++ components/Pages/Blog/Comments/index.tsx
@@ -1,10 +1,22 @@
-import { FC } from "react";
+import { useEffect, FC } from "react";
+import { useRouter } from "next/router";
 
 interface PropTypes {
   url: string;
 }
 
+declare const window: Window &
+  typeof globalThis & {
+    FB: any;
+  };
+
 const BlogComments: FC<PropTypes> = ({ url }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    window.FB.XFBML.parse();
+  }, [router.pathname]);
+
   return (
     <div
       className="fb-comments"
