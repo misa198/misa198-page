8c030f3c039cc2c9e8aa1bad65a8f21b1bbaca92
Thanh Vu <csnsjh@gmail.com>
2021-08-01 15:36:01 +0700

    get blog

--- services/blogs-api.service.ts
+++ services/blogs-api.service.ts
@@ -2,11 +2,15 @@ import axios, { AxiosResponse } from "axios";
 import queryString from "query-string";
 
 import { GetBlogsDto } from "server/dtos/blogs/get-blogs.dto";
-import { GetBlogsResponse } from "types/Blog";
+import { Blog, GetBlogsResponse } from "types/Blog";
 
 export const getBlogs = async (
   query: GetBlogsDto,
 ): Promise<AxiosResponse<GetBlogsResponse>> => {
   const stringifiedQuery = queryString.stringify(query);
   return axios.get(`/api/blogs?${stringifiedQuery}`);
+};
+
+export const getBlog = async (slug: string): Promise<AxiosResponse<Blog>> => {
+  return axios.get(`/api/blogs/${slug}`);
 };
