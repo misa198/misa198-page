f08c7b234d963c3f7b015c023d86ebce208c6e5c
Thanh Vu <csnsjh@gmail.com>
2021-08-01 13:16:29 +0700

    progress bar

---
+++ components/ProgressBar/index.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+import NextNprogress from "nextjs-progressbar";
+
+import theme from "themes/styled-components-theme";
+
+const ProgressBar: FC = () => {
+  return (
+    <NextNprogress
+      color={theme.colorPrimary}
+      startPosition={0.3}
+      stopDelayMs={200}
+      height={3}
+    />
+  );
+};
+
+export default ProgressBar;
--- layouts/index.tsx
+++ layouts/index.tsx
@@ -2,13 +2,15 @@ import { FC } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-import { Main, LayoutWrapper } from "./styled";
+import ProgressBar from "components/ProgressBar";
 import Header from "components/Header";
 import Footer from "components/Footer";
+import { Main, LayoutWrapper } from "./styled";
 
 const App: FC = ({ children }) => {
   return (
     <LayoutWrapper>
+      <ProgressBar />
       <Header />
       <Main>{children}</Main>
       <Footer />
