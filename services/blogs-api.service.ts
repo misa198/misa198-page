b8d053fc6a73020456de28857ca44ac40264fab7
Thanh Vu <csnsjh@gmail.com>
2021-08-02 10:40:15 +0700

    update

--- services/blogs-api.service.ts
+++ services/blogs-api.service.ts
@@ -8,13 +8,17 @@ const baseApiUrl = `${
   process.env.NEXT_PUBLIC_APP_DOMAIN || process.env.APP_DOMAIN
 }/api`;
 
+const axiosApi = axios.create({
+  baseURL: baseApiUrl,
+});
+
 export const getBlogs = async (
   query: GetBlogsDto,
 ): Promise<AxiosResponse<GetBlogsResponse>> => {
   const stringifiedQuery = queryString.stringify(query);
-  return axios.get(`${baseApiUrl}/blogs?${stringifiedQuery}`);
+  return axiosApi.get(`${baseApiUrl}/blogs?${stringifiedQuery}`);
 };
 
 export const getBlog = async (slug: string): Promise<AxiosResponse<Blog>> => {
-  return axios.get(`${baseApiUrl}/blogs/${slug}`);
+  return axiosApi.get(`${baseApiUrl}/blogs/${slug}`);
 };
