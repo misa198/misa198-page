5a648eb533e3d2d52427874d1a9d367252984ce8
Thanh Vu <csnsjh@gmail.com>
2021-09-17 04:13:14 +0700

    update

--- components/Pages/Blog/Content/styled.tsx
+++ components/Pages/Blog/Content/styled.tsx
@@ -4,14 +4,20 @@ export const BlogContentWrapper = styled.div`
   width: 100%;
   line-height: 1.8;
 
+  .anchor {
+    display: none !important;
+  }
+
   * {
     max-width: 100%;
-    font-family: "Roboto" !important;
+    font-family: "Roboto";
+    line-height: 1.5 !important;
   }
 
   code {
-    font-family: "SFMono-Regular", "Consolas", "Liberation Mono", "Menlo",
-      "monospace" !important;
+    * {
+      font-family: "Source Code Pro" !important;
+    }
   }
 
   img {
@@ -23,7 +29,6 @@ export const BlogContentWrapper = styled.div`
   }
 
   p {
-    line-height: 1.5;
     font-size: 1rem;
   }
 `;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -26,6 +26,10 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap"
+            rel="stylesheet"
+          />
           {/* Global site tag (gtag.js) - Google Analytics */}
           <script
             async
