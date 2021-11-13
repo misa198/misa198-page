bccbd892b77b3e0014d1cd8b73879551895d7043
Thành Vũ <csnsjh@gmail.com>
2021-11-14 01:09:41 +0700

    chore: remove unless components

--- components/Pagescsdc/Blogs/BlogListLoading/styled.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const BlogListWrapper = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  animation: appear 200ms;
-`;
--- components/Pagination/styled.tsx
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-import { Pagination } from "@material-ui/lab";
-
-export const PaginationWrapper = styled(Pagination)`
-  width: fit-content;
-`;
