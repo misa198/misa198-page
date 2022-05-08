3c1ccfd1e16c181643deb2e405dd3e9fb9b39e2c
Thanh Vu <thanhvc.198@gmail.com>
2022-05-08 15:00:30 +0700

    fix: change github usernam

--- constants/config.ts
+++ constants/config.ts
@@ -1,4 +1,4 @@
-export const username = 'misa198';
+export const username = 'thanhvu1982';
 export const sheetUrl =
   'https://sheet.best/api/sheets/fca2036a-e839-4dd2-ad56-ad2d3c2c9976';
 export const domain = process.env.NEXT_PUBLIC_APP_DOMAIN;
