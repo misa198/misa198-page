232ca799f404146bb4a3cd5e8b898b6f11e5ab98
ThanhVC <thanhvc.198@gmail.com>
2022-10-23 22:06:44 +0700

    chore: init

--- app/apis/githubApi.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-import { ghPinRepoUrlApi, username } from '@constants/config';
-import { PinnedRepository } from '@models/PinnedRepository';
-
-export async function getPinnedRepositories(): Promise<PinnedRepository[]> {
-  const response = await axios.get(`${ghPinRepoUrlApi}/?username=${username}`);
-  return response.data;
-}
